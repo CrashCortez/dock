2d868c559f1a70352884ec496833d3cb6555f978
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2020-06-12 08:09:34 -0700

    Update setup.sh

--- mpv_mods/setup.sh
+++ mpv_mods/setup.sh
@@ -3,9 +3,9 @@ cd /home/pi/dock/mpv_mods/
 sudo chown pi:pi *.sh
 sudo chmod a+x *.sh
 cd
-sudo cp /home/pi/dok/mpv_mods/Etc_Systemd_user/* /etc/systemd/user/
-sudo cp /home/pi/dok/mpv_mods/etc_udev_rules.d/* /etc/udev/rules.d/
-sudo cp /home/pi/dok/mpv_mods/close.sh /home/pi/scripts/
-sudo cp /home/pi/dok/mpv_mods/launchscripts /home/pi/scripts/
-#sudo cp /home/pi/dok/mpv_mods/vidware /home/pi/
-sudo systemctl enable /etc/systemd/user/vita.service && sudo systemctl start vita.service
+sudo cp /home/pi/dock/mpv_mods/Etc_Systemd_user/* /etc/systemd/user/
+sudo cp /home/pi/dock/mpv_mods/etc_udev_rules.d/* /etc/udev/rules.d/
+sudo cp /home/pi/dock/mpv_mods/close.sh /home/pi/scripts/
+sudo cp /home/pi/dock/mpv_mods/launchscripts /home/pi/scripts/
+#sudo cp /home/pi/dock/mpv_mods/vidware /home/pi/
+sudo systemd enable /etc/systemd/user/vita.service && sudo systemd start vita.service
