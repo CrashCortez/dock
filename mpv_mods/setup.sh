4881e92485fe0df8a67ab06782846034226d5f6f
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2022-07-26 08:45:59 -0700

    Update setup.sh

--- mpv_mods/setup.sh
+++ mpv_mods/setup.sh
@@ -9,3 +9,4 @@ sudo cp /home/pi/dock/mpv_mods/close.sh /home/pi/scripts/
 sudo cp /home/pi/dock/mpv_mods/launchscripts /home/pi/scripts/
 #sudo cp /home/pi/dock/mpv_mods/vidware /home/pi/
 sudo systemd enable /etc/systemd/user/vita.service && sudo systemd start vita.service
+sudo systemd enable /etc/systemd/user/deck.service && sudo systemd start deck.service
